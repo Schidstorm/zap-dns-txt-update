79bc873cb6aa4451b5f71ec5f91c2ea1a0a36e60
Dominik Schidlowski <dominik@schidlowski.eu>
2020-02-22 16:22:44 -0500

    remove comment

--- change-txt.sh
+++ change-txt.sh
@@ -13,7 +13,6 @@ function changeTxt() {
     ttl="${5-"3600"}"
 
     csrfToken="`extractCsrfToken https://zap-hosting.com/de/customer/domain/show/$domainId/dns/`"
-    #csrf_token_zap=8c0ac66808702a15b4c6a0d53347de59a0d25d24af640e17&record_name=test&record_type=TXT&record_value=123456&record_ttl=3600&record_id=300081&cmd=EDIT&saveDnsZone=
     request \
         -d "csrf_token_zap=$csrfToken&record_name=$txtName&record_type=TXT&record_value=$value&record_ttl=$ttl&record_id=$recordId&cmd=EDIT&saveDnsZone=" \
         https://zap-hosting.com/de/customer/domain/show/47060/dns/ > /dev/null
